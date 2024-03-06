a5acda6273eecc08e1d7c77988fdd397dc949e31
Vinipcsl <viniciuspcsl@gmail.com>
2024-03-06 00:15:37 -0300

    Torquei o fundo coloquei o text area transparente e o onde recebe a tradução está meio transparente tambem.

--- controller.js
+++ controller.js
@@ -1,57 +1,39 @@
-const textoInput = document.getElementById('textoInput');
-const botaoCriptografar = document.getElementById('botaoCriptografar');
-const botaoDescriptografar = document.getElementById('botaoDescriptografar');
-const output = document.getElementById('output');
-
-const criptografar = (text) => {
-	
-    const criptografia = {
-        e: "enter",
-        i: "imes",
-        a: "ai",
-        o: "ober",
-        u: "ufat"
+function criptografarLetra(inputTexto){
+    const cripto ={
+       a: "ai",
+       e: "enter",
+       i: "imes",
+       o: "ober",
+       u: "ufat"
     };
 
     let textoCriptografado = '';
 
-    for (let i = 0; i<text.lenght; i++){
-        const char = text[i];
-        const substituir = criptografia[char] || char;
-        textoCriptografado += substituir;
-    }
+    for (let i = 0; i<inputTexto.length; i++){
+       const char = inputTexto[i];
+       const substituir = cripto[char] || char;
+       textoCriptografado += substituir;
+   }
 
-	return textoCriptografado;
+   return textoCriptografado;
 }
 
-const descriptografar = (text) => {
 
-    const descriptografia ={
-        ai: "a",
-        enter: "e",
-        imes: "i",
-        ober: "o",
-        ufat: "u"
-    };
 
-    let textoDescriptografado = '';
 
-    
-    for (let i = 0; i<text.lenght; i++){
-        const char = text[i];
-        const substituir = descriptografia[char] || char;
-        textoDescriptografado += substituir;
-    }
-    
-    return textoDescriptografado;
+function criptografar() {
+   const inputTexto = document.getElementById("textoInput").value;
+   const textoCriptografado = criptografarLetra(inputTexto.toLowerCase());
+   const labelTexto = document.getElementById("labelResposta");
+   labelTexto.textContent = textoCriptografado;
+
+   var classeImagem = document.querySelector('.meio_imagem');
+
+   if(classeImagem.style.display === 'block'){
+    classeImagem.style.display = 'none';
+   }
+   else{
+    classeImagem.style.display = 'none';
+   }
 }
 
-encryptButton.addEventListener('click', () => {
-	const textoCriptografado = criptografar(textInput.value);
-	output.textContent = textoCriptografado;
-});
-
-decryptButton.addEventListener('click', () => {
-	const textoDescriptografado = descriptografar(textInput.value);
-	output.textContent = textoDescriptografado;
-});
